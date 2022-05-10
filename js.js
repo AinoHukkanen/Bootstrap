b6d7beb92f7d710456a23e3f24e261eb2a33d687
AinoHukkanen <n1huai00@students.oamk.fi>
2022-05-10 15:09:41 +0300

    SASS-muutoksia tehty

--- js.js
+++ js.js
@@ -4,7 +4,6 @@ let button = document.getElementById("lajitteluhattu");
 
 p.addEventListener("mouseenter", changeColor);
 p.addEventListener("mouseleave", changeColor);
-button.addEventListener("click", lajitteluhattu);
 button.addEventListener("click", tylypahka);
 
 function changeColor() {
@@ -28,35 +27,6 @@ function tylypahka(){
 
 
 
-function vastaus() {
-
-    if(document.getElementById("1a").checked) {
-         document.getElementById("results").innerHTML = " OIKEA VASTAUS "
-         document.getElementById("submit").disabled = true;
-         document.getElementById("1b").disabled = true;
-         document.getElementById("1c").disabled = true;
-    
-    if(document.getElementById("2a").checked) {
-            document.getElementById("results").innerHTML = " OIKEA VASTAUS "
-            document.getElementById("submit").disabled = true;
-            document.getElementById("2b").disabled = true;
-            document.getElementById("2c").disabled = true;
 
 
-    if(document.getElementById("3a").checked) {
-                document.getElementById("results").innerHTML = " OIKEA VASTAUS "
-                document.getElementById("submit").disabled = true;
-                document.getElementById("3b").disabled = true;
-                document.getElementById("3c").disabled = true;
 
-    if(document.getElementById("4a").checked) {
-                    document.getElementById("results").innerHTML = " OIKEA VASTAUS "
-                    document.getElementById("submit").disabled = true;
-                    document.getElementById("4b").disabled = true;
-                    document.getElementById("4c").disabled = true;
-
-    }
-}
-    }
-}
-}
