e96a55630000da73fdc422c11a94f34ea0715637
AinoHukkanen <n1huai00@students.oamk.fi>
2022-05-14 10:41:37 +0300

    Lisätty muutoksia koodiin

--- hahmo.js
+++
@@ -1,9 +0,0 @@
-const urlToFetch = "https://fedeperin-harry-potter-api-en.herokuapp.com/characters"
-
-fetch(urlToFetch)
-	.then((res) => res.json())
-	.then((data) => {
-		// It brings all data at the API and it shows it at the console
-		console.log(data)
-	})
-	.catch((e) => console.log(e));
